a167bf05aae78d34c0f955af1ce769adfb3f564d
Charline Vincent <charline.vincent1992@gmail.com>
2024-10-06 19:11:51 +0200

    modale

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function motaphoto_styles() {
     wp_enqueue_style('theme-css', get_template_directory_uri() . '/css/theme.css', array(), '1.0', 'all');
+    wp_enqueue_script('script', get_stylesheet_directory_uri() . '/js/scripts.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'motaphoto_styles');
 
