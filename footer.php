7efc6089043338af787d4468c77882f64c949391
Charline Vincent <charline.vincent1992@gmail.com>
2024-12-19 15:29:01 +0100

    template part : Lightbox

--- footer.php
+++ footer.php
@@ -2,31 +2,7 @@
 
 <?php include get_template_directory() . '/template-parts/modale-contact.php'; ?>
 
-
-<!-- Lightbox Photo -->
-<div class="lightbox-container">
-    <span class="btn-close">X</span>
-    <div class="lightbox-wrapper">
-        <div class="left-arrow">
-            <img class="hover-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/images/lightbox-arrow-left.png" alt="Précédent">
-            <p class="text-arrow">Précédente</p>
-        </div>
-        <div class="lightbox-block">
-            <div class="lightbox-img">
-                <img src="" class="middle-image" alt="Image en plein écran">
-            </div>
-            <div class="info-photo">
-                <span id="lightbox-reference" class="info-left"></span>
-                <span id="lightbox-category" class="info-right"></span> <!-- Affiche la catégorie -->
-            </div>
-        </div>
-        <div class="right-arrow">
-            <p class="text-arrow">Suivante</p>
-            <img class="hover-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/images/lightbox-arrow-right.png" alt="Suivant">
-        </div>
-    </div>
-</div>
-
+<?php include get_template_directory() . '/template-parts/lightbox.php';?>
 
 <!-- Footer -->
 
